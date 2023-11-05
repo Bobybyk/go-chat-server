94c7830c62ea075311fc549a9c71f751c221979f
Bobybyk <matt.lefranc@gmail.com>
2023-11-05 16:27:44 +0100

    Update WebSocket and CORS configuration

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -9,12 +9,22 @@ import (
 )
 
 /** Configuration du gestionnaire d'itinéraire pour la connexion WebSocket, 
+ * configuration du gestionnaire CORS,
  * démarrage d'une goroutine pour gérer les messages entrants, 
  * sert les fichiers statiques ou l'interface utilisateur (plus tard), 
- * écoute sur le port 8081
+ * écoute sur le port 8080
+ * 
+ * Note sur le gestionnaire CORS:
+ * Toutes les demandes HTTP et WebSocket reçues par le serveur passeront d'abord par le gestionnaire CORS, 
+ * qui vérifiera si elles sont autorisées en fonction des options spécifiées. 
+ * Si une demande correspond aux critères définis (origine, méthodes, en-têtes), elle sera autorisée.
+ * 
+ * Avec cette configuration, le serveur acceptera des connexions WebSocket depuis n'importe quel domaine, 
+ * ce qui facilite la connexion depuis d'autres machines ou sites web. 
+ * Possible d'ajuster les options CORS en fonction des besoins en sécurité et de gestion des origines.
  */
 func main() {
-    // Créez un routeur avec gorilla/mux
+    // Créez un routeur avec gorilla/mux pour gérer les routes
     r := http.NewServeMux()
 
     // Configuration de CORS
@@ -22,13 +32,13 @@ func main() {
     originsOk := handlers.AllowedOrigins([]string{"*"})
     methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
 
-    // Ajoutez un gestionnaire WebSocket avec le routeur
+    // Ajout d'un gestionnaire WebSocket avec le routeur
     r.HandleFunc("/ws", server.HandleConnections)
 
-    // Créez un gestionnaire CORS avec les options configurées
+    // Cration d'un gestionnaire CORS avec les options configurées
     corsHandler := handlers.CORS(originsOk, headersOk, methodsOk)(r)
 
-    // Utilisez le gestionnaire CORS comme gestionnaire racine
+    // Gestionnaire CORS utilisé comme gestionnaire racine
     http.Handle("/", corsHandler)
 
     // Démarrage du serveur sur le port 8080
